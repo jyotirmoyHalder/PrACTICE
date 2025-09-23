a489d696a5ccd2fe0bc272fe3ce5ed94f73b3162
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2025-09-23 21:36:44 +0600

    swiftfirst

--- PrACTICE/main.swift
+++ PrACTICE/main.swift
@@ -7,5 +7,28 @@
 
 import Foundation
 
-print("Hello, World!")
+let arraySize = 5 // Read and dropteh array size line.
+// Knowing the size of the array beforehand is needed for some languages
+// But as a readLine() returns the whole line, Swift doesn't need it!
+var array = [Int]()
+array.reserveCapacity(arraySize)
+for i in 0..<arraySize {
+    array.append(solve(for: i))
+}
+
+let fiveZs = Array(repeating: "Z", count: 5)
+print(fiveZs)
+
+func solve(for index: Int) -> Int {
+    return index * 2
+}
+
+print(array)
+print(array)
+
+//array = readLine()!.components(separatedBy: " ").map{ Int($0)! }
+//let result = array.reduce(0, +)
+//print(result)
+//
+//print(1 &+ 1)
 
